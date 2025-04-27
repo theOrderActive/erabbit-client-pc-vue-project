a7f7f2dd792f53dc4d8651d682e77bdc6800e90a
theorder <2486711860@qq.com>
2025-04-27 23:59:14 +0800

    category

--- src/components/library/index.js
+++ src/components/library/index.js
@@ -1,41 +1,57 @@
-// 扩展vue原有的功能：全局组件，自定义指令，挂载原型方法，注意：没有全局过滤器。
-// 这就是插件
-// vue2.0插件写法要素：导出一个对象，有install函数，默认传入了Vue构造函数，Vue基础之上扩展
-// vue3.0插件写法要素：导出一个对象，有install函数，默认传入了app应用实例，app基础之上扩展
+// 其实就是vue插件，扩展vue功能，全局组件、指令、函数 （vue.30取消过滤器）
+// 当你在mian.js导入，使用Vue.use()  (vue3.0 app)的时候就会执行install函数
+// import XtxSkeleton from './xtx-skeleton.vue'
+// import XtxCarousel from './xtx-carousel.vue'
+// import XtxMore from './xtx-more.vue'
+// import XtxBread from './xtx-bread.vue'
+// import XtxBreadItem from './xtx-bread-item.vue'
 
-import XtxSkeleton from './xtx-skeleton.vue'
-import XtxCarousel from './xtx-carousel.vue'
-import XtxMore from './xtx-more.vue'
+// 导入library文件夹下的所有组件
+// 批量导入需要使用一个函数 require.context(dir,deep,matching)
+// 参数：1. 目录  2. 是否加载子目录  3. 加载的正则匹配
+const importFn = require.context('./', false, /\.vue$/)
+// console.dir(importFn.keys()) 文件名称数组
 
-import defaultImg from '@/assets/images/200.png'
-// 指令
+export default {
+  install (app) {
+    // app.component(XtxSkeleton.name, XtxSkeleton)
+    // app.component(XtxCarousel.name, XtxCarousel)
+    // app.component(XtxMore.name, XtxMore)
+    // app.component(XtxBread.name, XtxBread)
+    // app.component(XtxBreadItem.name, XtxBreadItem)
+
+    // 批量注册全局组件
+    importFn.keys().forEach(key => {
+      // 导入组件
+      const component = importFn(key).default
+      // 注册组件
+      app.component(component.name, component)
+    })
+
+    // 定义指令
+    defineDirective(app)
+  }
+}
+
 const defineDirective = (app) => {
-  // 图片懒加载指令
+  // 图片懒加载指令 v-lazyload
   app.directive('lazyload', {
+    // vue2.0 inserted函数，元素渲染后
+    // vue3.0 mounted函数，元素渲染后
     mounted (el, binding) {
-      const observer = new IntersectionObserver(([{ isIntersecting }]) => {
+      // 元素插入后才能获取到dom元素，才能使用 intersectionobserve进行监听进入可视区
+      // el 是图片元素  binding.value 图片地址
+      const observe = new IntersectionObserver(([{ isIntersecting }]) => {
         if (isIntersecting) {
-          observer.unobserve(el)
-          el.onerror = () => {
-            el.src = defaultImg
-          }
           el.src = binding.value
+          // 取消观察
+          observe.unobserve(el)
         }
       }, {
         threshold: 0.01
       })
-      observer.observe(el)
+      // 进行观察
+      observe.observe(el)
     }
   })
-}
-
-export default {
-  install (app) {
-    // 在app上进行扩展，app提供 component directive 函数
-    // 如果要挂载原型 app.config.globalProperties 方式
-    app.component(XtxSkeleton.name, XtxSkeleton)
-    app.component(XtxCarousel.name, XtxCarousel)
-    app.component(XtxMore.name, XtxMore)
-    defineDirective(app)
-  }
 }
